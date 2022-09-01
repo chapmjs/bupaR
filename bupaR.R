844815ac0725e2365d1a16053d5e3dd0911b1d16
John Chapman <chapmjs@gmail.com>
2022-09-01 17:01:15 -0600

    added resource links

--- bupaR.R
+++ bupaR.R
@@ -19,3 +19,28 @@ library (eventdataR)
 eventdataR::patients
 patients %>% mapping
 patients %>% summary
+
+patients %>% n_activities
+patients %>% n_activity_instances
+patients %>% n_cases
+patients %>% n_events
+patients %>% n_traces
+patients %>% n_resources
+
+
+patients %>% cases
+
+
+# Read XES-files
+data <- read_xes("https://bupar.net/eventdata/exercise1.xes")
+data
+
+read_xes_cases("https://bupar.net/eventdata/repairExample.xes")
+
+read_xes("https://bupar.net/eventdata/repairExample.xes")
+
+# Write XES-files
+args(write_xes)
+
+eventdataR::patients
+write_xes(patients, "patients.xes")
