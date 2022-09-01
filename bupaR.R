7bab835c854e5ffd9359273efcbe05ddcdac96d5
John Chapman <chapmjs@gmail.com>
2022-09-01 16:42:27 -0600

    added packages and first lines

--- bupaR.R
+++ bupaR.R
@@ -1,2 +1,21 @@
 # bupaR
 # https://campus.datacamp.com/courses/business-process-analytics-in-r/
+
+# Link to Cheatsheet:
+# https://bupar.net/materials/20170904%20poster%20bupaR.pdf
+
+# # Install Packages
+# install.packages("bupaR")
+# install.packages("edeaR")
+# install.packages("eventdataR")
+# install.packages("processmapR")
+# install.packages("processmonitR")
+# install.packages("xesreadR")
+# install.packages("petrinetR")
+
+library (bupaR)
+library (eventdataR)
+
+eventdataR::patients
+patients %>% mapping
+patients %>% summary
